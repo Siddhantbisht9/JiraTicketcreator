6946f466035bfcf53ed7a8173403c386326ceb00
Siddhant Bisht <siddhant.bisht@aboutsib.com>
2026-03-19 19:13:14 +0530

    Correct Files added

---
+++ edgeprofilestatic.ps1
@@ -0,0 +1,13 @@
+# If Edge is running right now (best option)
+(Get-Process msedge -ErrorAction SilentlyContinue | Select-Object -First 1).Path
+
+# Try common locations
+Get-Item "C:\Program Files\Microsoft\Edge\Application\msedge.exe" -ErrorAction SilentlyContinue    #needs to be as per your edge profile
+Get-Item "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ErrorAction SilentlyContinue
+Get-Item "$env:LOCALAPPDATA\Microsoft\Edge\Application\msedge.exe" -ErrorAction SilentlyContinue
+
+# If Edge is on PATH
+where.exe msedge
+
+# Registry (if present)
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe" /ve
